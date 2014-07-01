4bb5ce3a06f4d1651dc1df88908f81f31e1e4551
Integralist <mark.mcdx@gmail.com>
2014-07-01 09:46:11 +0100

    Clarify some parts of Docker and Containers that might not be clear enough

--- Sinatra/src/README.md
+++ Sinatra/src/README.md
@@ -1,8 +1,9 @@
 ```bash
+cd src
 docker build -t integralist/sinatra:v3 .
 docker images
 docker run -p 4567:4567 -d integralist/sinatra:v3
 docker ps
 docker logs {container_id}
-curl -i $(boot2docker ip):49161
+curl -i http://localhost:4567
 ```
--- Sinatra/src/app.rb
+++ Sinatra/src/app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 
 # Bind to ALL device interfaces
 # This is so app localhost can be accessed outside a Docker container
+# So although in the Vagrantfile we expose port 4567 to the host machine
+# we're not exposing the VM's localhost unless we set the application to
+# bind to all the available interfaces
 set :bind, '0.0.0.0'
 
 get '/' do
