4b03ab56de2a8499a094174293e4bf5e778285af
Integralist <mark.mcdx@gmail.com>
2014-08-23 15:38:10 +0100

    Modify comment

--- Sinatra/src/app.rb
+++ Sinatra/src/app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 # Bind to ALL device interfaces
 # This is so app localhost can be accessed outside a Docker container
-# So although in the Vagrantfile we expose port 4567 to the host machine
+# So although in the Dockerfile we expose port 4567 to the host machine
 # we're not exposing the VM's localhost unless we set the application to
 # bind to all the available interfaces
 set :bind, '0.0.0.0'
